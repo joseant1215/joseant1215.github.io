bbbefcbd603c8aeb77682161f39b5ec97da81a9e
joseant1215 <80193930+joseant1215@users.noreply.github.com>
2026-08-13 20:01:22 -0500

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Preparado para https://github.com/joseant1215/joseant1215.github.io
-  // Si usas otro nombre de repositorio, cambia '/' por '/NOMBRE-REPO/'.
   base: '/',
 })
